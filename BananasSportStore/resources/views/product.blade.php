d4e0bae421270f8d90cb172a928d59acb6d5e5a7
haulele <20520493@gm.uit.edu.vn>
2022-11-21 20:29:52 +0700

    Merge branch 'main' of https://github.com/haulele/Sport-Store into Linh-Nhâm

---
+++ BananasSportStore/resources/views/product.blade.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BANASPORTS</title>
+    <link href="{{ url('css/product.css') }}" type="text/css" rel="stylesheet"> 
+    <link rel="stylesheet" href="{{ url('font/themify-icons-font/themify-icons/themify-icons.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href='https://fonts.googleapis.com/css?family=Work Sans' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+</head>
+<body >
+    <div class="site-blocks-cover" style="background-image: url('img/Rectangle15.png');">
+        <div class="container">
+            <div class="site-img">
+                    <div class="caption">
+                        <h3><b>TẤT CẢ SẢN PHẨM</b></h3>
+                    </div>
+            </div>
+        </div>
+    </div> 
+<div class="filter">
+    <div class="container-fluid">
+      <div class="row justify-content-center py-4">
+            <div class="col-lg-1 col-md-2 col-sm-4 col text-center py-1">
+                <p><a href="#" class="btn btn-outlined btn-info">Áo</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-md-2 col-sm-4 col text-center py-1">
+                
+                <p><a href="#" class="btn btn-outlined btn-info">Quần</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-md-2 col-sm-4 col text-center py-1">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Giày dép</a>
+                </p>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col text-center ml-lg-4 py-1">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Dụng cụ thể thao</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-md-2 col-sm-4 col text-center py-1">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info ">Gym</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-sm-4 text-center col col-md-2 py-1">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Tennis</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-sm-4 text-center col col-md-2 py-1">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Bóng đá</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-sm-4 text-center col col-md-2 py-1 ml-lg-3">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Bóng rổ</a>
+                </p>
+            </div>
+            <div class="col-lg-1 col-sm-4 text-center col col-md-2 py-1 ml-lg-3">
+                        
+                <p><a href="#" class="btn btn-outlined btn-info">Chạy bộ</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="ftco-section bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="product-list">
+                <div class="row ">
+                    <div class="sort py-4 text-right">
+                        <select name="sort-name" id="sort-select" style="padding: 10px ;">
+                            <option value="1">Sắp xếp: Thứ tự mặc định</option>
+                            <option value="2">Tên A-Z</option>
+                            <option value="3">Tên Z-A</option>
+                            <option value="4">Giá từ thấp đến cao</option>
+                            <option value="5">Giá từ cao đến thấp</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3   py-3 ">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block"  src="{{ url('img/Rectangle13.png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#" class="card-name">Áo Khoác Thể thao Nam PUMA</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">1.120.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block " src="{{ url('img/Rectangle13(1).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#" class="card-name">Tạ Tay PROCASE Sắt Bọc Nhựa 3 Kg</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">295.000</span></p>
+                                    </div> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(2).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#" class="card-name">Balo NIKE  Utility Elite màu đen</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">2.530.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(3).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#" class="card-name">Áo croptop tập luyện nữ PUMA</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">630.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(4).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Giày Tập Luyện Unisex PUMA</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">2.500.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(5).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Áo Khoác Thể thao Nam PUMA</a></h3>
+                                
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">2.790.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(6).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Quần legging nữ NIKE màu đen  </a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">1.005.000</span></p>
+                                    </div>  
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(7).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Balo tập luyện ERKE màu xanh</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">860.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(8).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Áo Bra tập luyện nữ ADIDAS</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">715.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(9).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Băng ống tay tập luyện ZAMST</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">965.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(10).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Áo croptop tập luyện nữ PUMA</a></h3>
+                                
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">630.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(11).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Áo Polo nam LIVAN MILER</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">360.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(12).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Nón Lưỡi Trai REEBOK</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">440.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(13).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Dép Lifestyle unisex PUMA màu xanh</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">895.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(14).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Thảm YOGA tập luyện PUMA màu đen</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">919.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+                        <div class="product" style="background-color: #E8EBF4; border-radius: 30px; height: 460px;">
+                            <a href="#" class="img-prod"><img class="img-fluid mx-auto p-3 d-block" src="{{ url('img/Rectangle13(15).png')}}" alt="Colorlib Template">
+                            </a>
+                            <div class="text py-3 px-3">
+                                <h3><a href="#"  class="card-name">Set đồ tập tay ngắn màu xám</a></h3>
+                                    <div class="pricing">
+                                        <p class="price"><span class="mr-2 price-dc">650.000</span></p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pagination no-gutters align-items-center justify-content-center p-5">
+                        <ul>
+                            <a href="#"><li>&lt</li></a>
+                            <a class="active" href="#"><li>1</li></a>
+                            <a href="#" style="border: 2px solid #6D87E3;"><li>2</li></a>
+                            <a href="#" style="border: 2px solid #6D87E3;"><li>3</li></a>
+                            <a href="#" class="d-none d-sm-inline-block" style="border: 2px solid #6D87E3;"><li>4</li></a>
+                            <a href="#" class="d-none d-sm-inline-block"  style="border: 2px solid #6D87E3;"><li>5</li></a>
+                            <a href="#" class="d-none d-sm-inline-block" style="border: 2px solid #6D87E3;"><li>6</li></a>
+                            <a href="#"><li>></li></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
